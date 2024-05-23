8caaa098d8ff786db5e58ad1a119ef25a511add4
Joe Earley <joe.x.earley@nz.pwc.com>
2024-05-23 16:04:42 +1200

    debug

--- validate/index.js
+++ validate/index.js
@@ -49,9 +49,7 @@
         }
         // execute the validation in the SF instance of the package
         const commandToExecute = `./${constants_1.DEFAULT_SFDX_CLI_INSTALLATION_FOLDER}/bin/sf`;
-        const params2 = [];
-        params2.push('update');
-        (0, exec_1.default)(commandToExecute, params2).then((result) => {
+        (0, exec_1.default)(commandToExecute, params).then((result) => {
           // parsed the result
           const parsedResult = JSON.parse(result);
 
